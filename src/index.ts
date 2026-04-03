cafe9aedee6e7e0f184c4b58082f5e7f6524c9ed
Podexus23 <santon23@gmail.com>
2026-04-03 15:59:26 +0300

    feat: add mongo db connection remove tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import 'dotenv/config';
+import { connectDB } from './config/db.js';
+import Fastify from 'fastify';
 
-export function main() {
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', () => {
+  return { hello: 'data' };
+});
+
+export async function main() {
   console.log(`Main is running on ${process.env.PORT}`);
+  await connectDB();
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
 
-main();
+await main();
